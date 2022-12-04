6d46418c143c6304bb4b938112899de5924af826
bxo11 <krzysiek.klim1999@gmail.com>
2022-12-04 19:03:47 +0100

    enigma update

--- enigma/MPIEnigmaBreaker.cpp
+++ enigma/MPIEnigmaBreaker.cpp
@@ -2,8 +2,12 @@
 #include "MPIEnigmaBreaker.h"
 #include "cmath"
 #include "mpi.h"
+
+const uint EXPECTED_MESSAGE_LENGTH = 50;
+const uint MESSAGE_LENGTH = 3000;
 void incrementTab(uint *tab, int lenght, int max, int increase)
 {
+	// cout<<"rotors: "<<lenght<<", maxValue: "<<max<<endl;
 	tab[lenght - 1] += increase;
 	for (int i = lenght - 1; i > 0; i--)
 	{
@@ -12,9 +16,41 @@ void incrementTab(uint *tab, int lenght, int max, int increase)
 			tab[i - 1]++;
 			tab[i] -= max + 1;
 		}
+		// cout<<tab[i]<<" ";
+	}
+	// cout<<endl;
+}
+
+void dekodowanie(int value, uint *r, int PODSTAWA, int ROTORS)
+{
+	int stanRotora;
+	int x_do_n;
+	// cout<<"rotors: "<<ROTORS<<", maxValue: "<<PODSTAWA<<endl;
+	for (int i = ROTORS - 1; i >= 0; i--)
+	{
+		x_do_n = pow(PODSTAWA, i);
+		stanRotora = value / x_do_n;
+		value -= stanRotora * x_do_n;
+		r[i] = stanRotora;
 	}
 }
 
+int pow(int x, int n)
+{
+	int iloczyn = 1;
+	for (int i = 0; i < n; i++)
+		iloczyn *= x;
+	return iloczyn;
+}
+
+void pp(uint *r, int size)
+{
+	for (int i = size; i >= 0; i--)
+	{
+		cout << r[i];
+	}
+	cout << endl;
+}
 MPIEnigmaBreaker::MPIEnigmaBreaker(Enigma *enigma, MessageComparator *comparator) : EnigmaBreaker(enigma, comparator)
 {
 }
@@ -54,6 +90,14 @@ int forwardStopSignal(MPI_Request *root_recv_stop, MPI_Request *all_recv_stop, i
 	return 0;
 }
 
+void copy2(uint *source, uint sourceLength, uint *destination, uint destinationLength)
+{
+	uint shift = sourceLength - destinationLength;
+
+	for (uint pos = 0; pos < destinationLength; pos++)
+		destination[pos] = source[pos + shift];
+}
+
 void MPIEnigmaBreaker::crackMessage()
 {
 	uint rotorLargestSetting = enigma->getLargestRotorSetting();
@@ -64,6 +108,29 @@ void MPIEnigmaBreaker::crackMessage()
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	// MPI_Bcast(&messageLength, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	uint *messageToDecode2 = new uint[MESSAGE_LENGTH];
+	uint *expectedMessage2 = new uint[EXPECTED_MESSAGE_LENGTH];
+
+	if (rank == MPI_ROOT_PROCESS_RANK)
+	{
+		for (int i = 0; i < messageLength; i++)
+		{
+			messageToDecode2[i] = messageToDecode[i];
+		}
+	}
+
+	MPI_Bcast(messageToDecode2, MESSAGE_LENGTH, MPI_INT, 0, MPI_COMM_WORLD);
+
+	// pp(messageToDecode,EXPECTED_MESSAGE_LENGTH);
+
+	setMessageToDecode(messageToDecode2, MESSAGE_LENGTH);
+	copy2(messageToDecode2, MESSAGE_LENGTH, expectedMessage2, EXPECTED_MESSAGE_LENGTH);
+	setSampleToFind(expectedMessage2, EXPECTED_MESSAGE_LENGTH);
+
+	// pp(messageToDecode2,EXPECTED_MESSAGE_LENGTH);
 
 	uint *r = new uint[MAX_ROTORS];
 	for (int j = 0; j < rotors; j++)
@@ -81,16 +148,19 @@ void MPIEnigmaBreaker::crackMessage()
 		MPI_Ibarrier(MPI_COMM_WORLD, &all_recv_stop);
 	}
 
-	int LARGEST_ROTOR_SETTING = 10;
-	for(int i=rank;i<pow(LARGEST_ROTOR_SETTING+1,rotors);i+=size)
+	for (int i = rank; i < pow(rotorLargestSetting+1, rotors); i += size)
 	{
-		incrementTab(r, rotors, rotorLargestSetting, size);
+		dekodowanie(i, r, rotorLargestSetting, rotors);
+		// incrementTab(r, rotors, rotorLargestSetting, size);
+		// pp(r,rotors);
 		repeats++;
-		if (solutionFound(r))
+		if (solutionFound(r)==true)
 		{
 			stopCond = 1;
 			// sending solution to main thread
-			MPI_Isend(r, rotors, MPI_INT, 0, 0, MPI_COMM_WORLD, &sendRequest);
+			pp(r,rotors);
+			MPI_Isend(r, rotors, MPI_INT, MPI_ROOT_PROCESS_RANK, 0, MPI_COMM_WORLD, &sendRequest);
+			
 		}
 		int found = stopCond;
 		if (found)
@@ -126,9 +196,32 @@ void MPIEnigmaBreaker::crackMessage()
 			}
 		}
 	};
+
+	if (rank == MPI_ROOT_PROCESS_RANK)
+	{
+		MPI_Status status;
+		uint *res = new uint[rotors];
+
+		cout << "czekam na wynik" << endl;
+		MPI_Recv(res, rotors, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+		cout << "Result from: " << status.MPI_SOURCE << endl;
+		for (uint rotor = 0; rotor < rotors; rotor++)
+		{
+			rotorPositions[rotor] = res[rotor];
+		}
+		enigma->setRotorPositions(rotorPositions);
+		uint *result2 = new uint[rotors];
+		getResult(result2);
+		showUint(rotorPositions, rotors);
+	}
+
 	cout << rank << ": " << repeats << " iterations" << endl;
 
 	delete[] r;
+}
+
+void setRootResutl()
+{
 }
 
 bool MPIEnigmaBreaker::solutionFound(uint *rotorSettingsProposal)
